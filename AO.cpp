ac68b86fcf9595ccd67fe392e42fc6c8346893a2
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-11 22:42:16 +0530

    Add files via upload
    The program written above icludes addition , subtraction , multiplication and division of two numbers that would be given by the user . Basically it is a program to perform arithematic operations in c++

---
+++ AO.cpp
@@ -0,0 +1,24 @@
+//program to perform arithematic operations
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int first ,second , add , substract , multiply;
+  float divide;
+  
+  cout<<"please enter two integers:";
+  cin<<first;
+  cin<<second;
+  
+  odd = first + second;
+  subtract = first - second;
+  multiply = first * second;
+  divide = first/(float) second;//typecasting
+  
+  cout<<endl<<"sum=" << add;
+  cout<<endl<<"difference ="<< subtract;
+  cout<<endl<<"multiplication =" << multiply;
+  cout<<endl<<"division="<<divide;
+  return 0;
+}
