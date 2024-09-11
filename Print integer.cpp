f3498da0df0b7134787b406620471c580ea9a676
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-11 16:25:54 +0530

    Add files via upload
    This is a program to print an integer as the output

---
+++ Print integer.cpp
@@ -0,0 +1,15 @@
+//program to print an integer
+//This is a preprocessor directive
+#include<iostream>
+using namespace std;
+int main()
+{
+  int number;
+  //to print the output
+  cout<<"enter a number:";
+  cin>> number ;
+  
+  cout<<"you entered"<<number;
+  
+  return 0;
+}
