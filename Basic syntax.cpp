8c9b1cbe2ae170211a6ce294cdff21a8eaceea94
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-11 16:15:40 +0530

    Add files via upload
    This is a basic syntax to print hello world in c++ 
    After we click run, the output will be displayed as hello world.

---
+++ Basic syntax.cpp
@@ -0,0 +1,14 @@
+// basic syntax to execute program in c++
+#include <iostream>
+using namespace std;
+
+/*this is my first c++ program */
+//main function
+
+int main()
+{
+  cout<<"hello world"<< endl;
+  
+}
+return 0;
+}
