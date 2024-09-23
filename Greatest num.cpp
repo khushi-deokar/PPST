a1da2ddfb1cbbd65b0b0aa9f332d30bc323cba0a
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-23 20:58:35 +0530

    Add files via upload
    In this program, three inputs are given and we have to check which one is the greatest among the three. For this nested if statement is used because there are more than 2 conditions provided therefore simple if-else statement could not be used.

---
+++ Greatest num.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a=2;
+  int b=4;
+  int c=1;
+  
+  if(a>=b && a>=c)
+  {
+    cout<<"a is greatest among the three"<<endl;
+    
+  }
+  else if(b>=a && b>=c)
+  {
+    cout<<"b is greatest among the three"<<endl;
+  }
+  
+  else
+  {
+    cout<<"c is greatest among the three"<<endl;
+  }
+  return 0;
+}
