053716a6fd2b96e39b797699fa6bb65f6672d2e6
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-24 20:34:05 +0530

    Add files via upload
    In the following program, the use of binary operators is demonstrated where 5 operators are used.

---
+++ Binary operators.cpp
@@ -0,0 +1,25 @@
+//program to demonstarte the binary operator
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a =8, b=3;
+  
+  //addition operator
+  cout<<"a+b="<<(a+b)<<endl;
+  
+  //substraction operator
+  cout<<"a-b="<<(a-b)<<endl;
+  
+  //multiplication operator
+  cout<<"a*b="<<(a*b)<<endl;
+  
+  //divisiin operator
+  cout<<"a/b="<<(a/b)<<endl;
+  
+  //modulo operator
+  cout<<"a%b="<<(a%b)<<endl;
+  
+  return 0;
+}
