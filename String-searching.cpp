b7f2f7a2754230cb90c79dc89e3313cbd49d35bc
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-24 21:29:32 +0530

    Add files via upload
    Searching operator in string searches whatever is asked in the code . Likewise , here we have asked to fing ‘world’.

---
+++ String-searching.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+string text = "Hello World";
+size_t pos = text.find("World"); 
+// Finds "World" in "Hello World"
+if (pos != string::npos)
+{
+cout << "'World' found at position: " << pos << endl;
+} 
+
+else
+{
+cout << "'World' not found" << endl; 
+}
+
+  return 0;
+}
