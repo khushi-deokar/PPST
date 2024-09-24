2bbf41f12da130743ecb2c8d282f0a6d425443c3
khushi-deokar <khushi.deokar24@sanjivani.edu.in>
2024-09-24 21:15:04 +0530

    Add files via upload
    Concatenation means joining in other words . So in this program aslo it is going to join the given . Here hello world will be connected as the output due to concatenation.

---
+++ Concatenation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+string str1 = "Hello";
+
+string str2 = "World";
+
+string result = str1 + " " + str2;
+
+cout << "Concatenated: " << result << endl;
+
+return 0;
+
+}
